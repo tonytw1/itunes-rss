a565d8439ef10d6d33497c80d8a1657924d31305
Tony McCrae <tony.mccrae@theguardian.com>
2024-03-13 12:47:19 +0000

    Demo logging.

--- app/com/gu/itunes/SecretKeeper.scala
+++ app/com/gu/itunes/SecretKeeper.scala
@@ -1,9 +1,15 @@
 package com.gu.itunes
 
+import org.slf4j.LoggerFactory
+
 object SecretKeeper {
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   def getApiKey(): Option[String] = {
-    Option(System.getenv("API_KEY"))
+    val maybeString = Option(System.getenv("API_KEY"))
+    logger.info("Got API KEY from ENV: " + maybeString)
+    maybeString
   }
 
 }
